3b88d5c136b7bfc3b81564ec42ec8aae9e211fcc
vivek-2000 <parthikpatel2000@gmail.com>
2021-08-06 00:47:59 +0530

    underdevelopment

---
+++ Forest-Fire-Prediction/app.py
@@ -0,0 +1,48 @@
+from flask import Flask,request, url_for, redirect, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model=pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def basic():
+    return render_template("login.html")
+
+database={'vivek':'123','james':'aac','karthik':'asdsf'}
+
+@app.route('/form_login',methods=['POST','GET'])
+
+def login():
+    name1=request.form['username']
+    pwd=request.form['password']
+    if name1 not in database:
+	    return render_template('login.html',info='Invalid User')
+    else:
+        if database[name1]!=pwd:
+            return render_template('login.html',info='Invalid Password')
+        else:
+	         return render_template('forest_fire.html',name=name1)
+@app.route('/')
+def hello_world():
+    return render_template("forest_fire.html")
+
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    print(int_features)
+    print(final)
+    prediction=model.predict_proba(final)
+    output='{0:.{1}f}'.format(prediction[0][1], 2)
+
+    if output>str(0.5):
+        return render_template('forest_fire.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output),bhai="kuch karna hain iska ab?")
+    else:
+        return render_template('forest_fire.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output),bhai="Your Forest is Safe for now")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
